32f1743f8665e3ea9b205205b1d13f16f45737ee
externalxdpro <externalxdpro@gmail.com>
2023-06-20 13:58:01 -0400

    Add comments and fix some bugs

--- SyntaxChecker.py
+++ SyntaxChecker.py
@@ -1,6 +1,12 @@
+# Sakib Ahmed
+# A tokenizer and kinda trash syntax checker
+
+# imports
 import string
 
 
+# An object containing data for a token
+# Has a type and a value
 class Token():
     def __init__(self, type: str, val: str):
         self.type = type
@@ -8,73 +14,111 @@ class Token():
 
 
 def Tokenizer(line):
+    """Tokenize the inputted line"""
+
+    # create an empty list of the tokens in the line
     tokens = []
 
+    # index for the current character in the line
     cur = 0
+    # keep looping until the end of the line
     while cur < len(line):
+        # set the character with the index
         char = line[cur]
-        type = ""
+        # set type to none
+        type = None
 
+        # if the character is any type of bracket, set the type to paren (parentheses)
         if char == "(" or char == ")" or char == "{" or char == "}" or char == "[" or char == "]":
             type = "paren"
+        # a comma and semicolon will be set to type seperator
         elif char == "," or char == ";":
             type = "seperator"
+        # a colon would be of type colon
         elif char == ":":
             type = "colon"
+        # if the character is an equals sign
         elif char == "=":
+            # if the next character is an equal sign,
+            # advance the index, and combine them into one string
+            # and set to type condition
             if cur != len(line) - 1 and line[cur + 1] == "=":
                 cur += 1
                 char += line[cur]
                 type = "condition"
+            # it would be an assignment if there is no second equals sign
             else:
                 type = "assignment"
+        # if the character is a plus/minus, the type can be
+        # assignment or number depending on what characters follow it
         elif char == "+" or char == "-":
-            if cur != len(line) - 1:
-                if line[cur + 1] == "=":
+            # if there is an equals sign after, it is an assignment operator
+            if cur != len(line) - 1 and line[cur + 1] == "=":
+                cur += 1
+                char += line[cur]
+                type = "assignment"
+            # if there is a number after, the type is number
+            elif cur != len(line) - 1 and line[cur + 1].isdigit():
+                num = char
+                cur += 1
+                char = line[cur]
+                while char.isdigit():
+                    num += char
                     cur += 1
-                    char += line[cur]
-                    type = "assignment"
-                if line[cur + 1].isdigit():
-                    num = ""
-                    while char.isdigit():
-                        num += char
-                        cur += 1
-                        char = line[cur]
-                    tokens.append(Token("number", num))
-                    continue
+                    char = line[cur]
+                # add the token to the list and continue
+                tokens.append(Token("number", num))
+                continue
+            # if there is nothing after, the type is math
             else:
                 type = "math"
+        # if the character is a greater/less than sign
+        # the type will be condition
         elif char == "<" or char == ">":
+            # if there is an equals sign after,
+            # that will be a part of the token as well
             if cur != len(line) - 1 and line[cur + 1] == "=":
                 cur += 1
                 char += line[cur]
             type = "condition"
+        # if the character is an exclamation mark with an equals sign after,
+        # then the type is condition
         elif char == "!":
             if cur != len(line) - 1 and line[cur + 1] == "=":
                 cur += 1
                 char += line[cur]
                 type = "condition"
+        # if the character is a digit, then the type is number
         elif char.isdigit():
+            # keep advancing the index to check for any digits after the current index
             num = ""
             while char.isdigit():
                 num += char
                 cur += 1
                 char = line[cur]
 
+            # add the token to the list and continue
             tokens.append(Token("number", num))
             continue
+        # if the character is a letter, the type is a name
+        # (could be the name of a function or a variable)
         elif char in string.ascii_letters:
+            # keep advancing the index to check for any letters after the current index
             name = ""
-            while char in string.ascii_letters:
+            while char in string.ascii_letters or char.isdigit():
                 name += char
                 cur += 1
                 char = line[cur]
 
+            # add the token to the list and continue
             tokens.append(Token("name", name))
             continue
+        # if the character is a quotation mark, then it must be a string
         elif char == "\"":
             name = ""
 
+            # keep advancing the pointer until the closing quotation mark is found
+            # meaning the end of the string
             while True:
                 name += char
                 cur += 1
@@ -84,36 +128,50 @@ def Tokenizer(line):
                     cur += 1
                     break
 
+            # add the token to the list and continue
             tokens.append(Token("string", name))
             continue
+        # if the character is a space or a line break then
+        # advance the index and continue
         elif char == " " or char == "\n":
             cur += 1
             continue
 
-        if type == "":
+        # if the type is none, there is an unknown character in the line
+        if type is None:
             raise SyntaxError(f"Unknown character: {char} in line: \n {line}")
 
+        # add the token to the list and advance the index
         tokens.append(Token(type, char))
         cur += 1
 
+    # return the list of tokens
     return tokens
 
 
 def SyntaxChecker(line):
+    # get a list of tokens for the passed-in line
     tokens = Tokenizer(line)
 
+    # if there are no tokens, exit the function
     if len(tokens) == 0:
         return
 
+    # the first token will be a name of a variable, or a keyword
     first = tokens[0]
     if first.type == "name":
-        # Handles for and foreach
+        # checks syntax for a for/foreach loop
         if first.val == "for":
+            # if there is a colon in the line, it is a foreach loop
             if ":" in line:
+                # the number of tokens must be 6 or 7
                 if len(tokens) != 6 and len(tokens) != 7:
                     raise SyntaxError(f"Incorrect syntax in line: \"{line}\"")
+                # specifies the order of the token types in a foreach loop
                 syntax = ["name", "paren", "name", "colon", "name", "paren", "paren"]
 
+                # loop through the lists and check if there are any inequalities
+                # if there is, there is a syntax error
                 for i in range(0, 5):
                     if tokens[i].type != syntax[i]:
                         print(tokens[i].type, tokens[i].val)
@@ -121,19 +179,27 @@ def SyntaxChecker(line):
             else:
                 if len(tokens) != 14 and len(tokens) != 15:
                     raise SyntaxError(f"Incorrect syntax in line: \"{line}\"")
+                # specifies the order of the token types in a for loop
                 syntax = ["name", "paren", "name", "assignment", "number",
                           "seperator", "name", "condition", "number",
                           "seperator", "name", "assignment", "number", "paren", "paren"]
 
+                # loop through the lists and check if there are any inequalities
+                # if there is, there is a syntax error
                 for i in range(0, 13):
                     if tokens[i].type != syntax[i]:
                         print(tokens[i].type, tokens[i].val)
                         raise SyntaxError(f"Incorrect syntax in line: \"{line}\"")
+        # checks syntax for a while loop
         elif first.val == "while":
+            # the number of tokens must be 6 or 7
             if len(tokens) != 6 and len(tokens) != 7:
                 raise SyntaxError(f"Incorrect syntax in line: \"{line}\"")
+            # specifies the order of the token types in a while loop
             syntax = ["name", "paren", "name", "condition", "number", "paren", "paren"]
 
+            # loop through the lists and check if there are any inequalities
+            # if there is, there is a syntax error
             for i in range(0, 5):
                 if tokens[i].type != syntax[i]:
                     print(tokens[i].type, tokens[i].val)
--- main.py
+++ main.py
@@ -25,85 +25,138 @@ numIndents = 0
 
 
 def MakeIndent():
+    """Create the specified number of indents."""
+
+    # allow access to the global variable
     global numIndents
+    # set the number of spaces in one indent
     indent = "    "
+    # multiply the spaces to create the specified number of indents
     return indent * numIndents
 
 
 def Copy(line):
+    """Directly copy a line from original file to compiled file."""
+
+    # allow access to the global variable
     global numIndents
+    # write the line from the original file to the compiled file
+    # with indents before it
     compFile.write(MakeIndent() + line)
 
 
 def For(line):
+    """Convert for loop in original file to compiled file."""
+
+    # allow access to the global variable
     global numIndents
 
+    # get the parameters for the for loop inbetween the brackets
     params = line[line.index("(") + 1:line.index(")")].split(",")
+
+    # strip each parameter
     for i in range(3):
         params[i] = params[i].strip()
 
-    # compFile = open(compPath, "a")
-    # with open(compPath, "a") as compFile:
+    # add the first parameter
+    # and the second parameter in a while loop
+    # with indents
     compFile.write(MakeIndent() + params[0] + "\n")
     compFile.write(MakeIndent() + f"while {params[1]}:\n")
 
+    # indent for the block of code under the while loop
     numIndents += 1
 
+    # get the next line from the origfile
     line = origFile.readline().strip()
+    # parse every line in the block
     ParseBlock(line)
+    # add the second third parameter at the end of the while loop
     compFile.write(MakeIndent() + params[2] + "\n")
 
+    # unindent at the end of the block
     numIndents -= 1
 
 
 def ForEach(line):
+    """Convert for-each loop in original file to compiled file."""
+
+    # allow access to the global variable
     global numIndents
 
+    # get the parameters for the for-each loop inbetween the brackets
     params = line[line.index("(") + 1:line.index(")")].strip().split(":")
+
+    # strip each parameter
     for i in range(2):
         params[i] = params[i].strip()
 
-    # with open(compPath, "a") as compFile:
+    # write the for loop into the compfile
     compFile.write(MakeIndent() + f"for {params[0]} in {params[1]}:\n")
 
+    # indent for the block under the for loop
     numIndents += 1
 
+    # read the next line from the origfile
     line = origFile.readline().strip()
+    # parse every line under the for loop
     ParseBlock(line)
 
+    # unindent after the block
     numIndents -= 1
 
 
 def While(line):
+    """Convert while loop in original file to compiled file."""
+
+    # allow access to the global variable
     global numIndents
 
+    # get the parameter for the while loop inbetween the brackets
     param = line[line.index("(") + 1:line.index(")")].strip()
 
+    # write the parameter into the compfile
     compFile.write(MakeIndent() + f"while {param}:\n")
 
+    # indent for the block under the while loop
     numIndents += 1
 
+    # read the next line from the origfile
     line = origFile.readline().strip()
+    # parse every line in the block under the while loop
     ParseBlock(line)
 
+    # unindent after the end of the while loop
     numIndents -= 1
 
 
 def Function(line):
+    """Convert function declaration in original file to compiled file."""
+
+    # allow access to the global variable
     global numIndents
+    # get the name of the function and the parameters of the function
     nameParams = line[line.index(" "):line.index(")") + 1].strip()
 
+    # write the name and the parameters into the compfile
     compFile.write(f"def {nameParams}:\n")
 
+    # indent for the body of the function
     numIndents += 1
 
+    # read the next line from the origfile
     line = origFile.readline().strip()
+    # parse the body of the function
     ParseBlock(line)
 
+    # unindent after the body of the function
     numIndents -= 1
 
 
 def ParseLine(line):
+    """Parse the inputted line depending on what it contains"""
+
+    # depending on the contents of the line, run the corresponding function
     if "for" in line:
         if ":" in line:
             ForEach(line)
@@ -118,32 +171,38 @@ def ParseLine(line):
 
 
 def ParseBlock(line):
+    """Parse the body of a for, for-each, while and function declaration"""
+
+    # allow access to the global variable
     global numIndents
+
+    # run until the closing curly bracket
     while line != "}":
+        # if the line only contains an opening curly bracket
+        # go to the next line
         if line == "{":
             line = origFile.readline().strip()
             continue
         else:
-            # compFile.write(MakeIndent(numIndents))
+            # parse each line and create a newline
             ParseLine(line)
             compFile.write("\n")
+        # read the next line
         line = origFile.readline().strip()
 
 
-# parse each line from the original file
+# check syntax and parse each line from the original file
 for line in origFile:
     SyntaxChecker.SyntaxChecker(line)
     ParseLine(line)
 
-
-# close the original and compiled files
+# close the original and compiled file
 origFile.close()
 compFile.close()
 
 # Execute everything from the newly "compiled" file
 with open("temp.py") as file:
-    # print(file.read())
     exec(file.read())
 
 # remove the temporary file
-# os.remove("temp.py")
+os.remove("temp.py")
